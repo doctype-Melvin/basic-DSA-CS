167cecd0975cea5d302919d8ed6fb3ba542aa3fd
doctype-Melvin <doctype.melvin@gmail.com>
2022-09-24 09:33:02 +0200

    fn creates and inserts nodes

--- src/binary-trees.js
+++ src/binary-trees.js
@@ -1,5 +1,48 @@
-const Tree = (array) => {
+const Tree = () => {
+
+    function makeNode(key, left = null, right = null) {
+      return {
+        key,
+        left,
+        right
+      }
+    }
+
+    function buildTree(array, start, end){
+      if (start > end) return null
+      let mid = parseInt((start + end) / 2)
+      let node = makeNode(array[mid]) 
+      node.left = buildTree(array, start, mid - 1)
+      node.right = buildTree(array, mid + 1, end)
+      return node
+    }
     
+    let root = null
+
+    function insert(key) {
+      let newNode = makeNode(key)
+
+      if (root === null){
+        root = newNode
+      }else{
+        insertNode(root, newNode)
+      }
+      return root
+    }
+
+    function insertNode(node, newNode){
+      if (newNode.key < node.key){
+        if (node.left === null){
+          node.left = newNode
+        }else insertNode(node.left, newNode)
+    }
+      if (newNode.key > node.key){
+        if (node.right === null) {
+          node.right = newNode
+        }else insertNode(node.right, newNode)
+      }
+  }
+
     function prettyPrint(node, prefix = "", isLeft = true) {
         if(node === null) return
         if (node.right !== null) {
@@ -12,6 +55,20 @@ const Tree = (array) => {
       }
 
     return {
-        prettyPrint        
+        makeNode,
+        buildTree,
+        prettyPrint,
+        insert     
     }
+}
+
+let bst = Tree()
+
+
+function randomArray(length){
+  let array = []
+  while (array.length < length) {
+    array.push(Math.floor(Math.random() * 667))
+  }
+  return array
 }
