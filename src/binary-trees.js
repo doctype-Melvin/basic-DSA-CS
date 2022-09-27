eec243cdf2118db5e533fa5d44e5df73c17085ab
doctype-Melvin <doctype.melvin@gmail.com>
2022-09-27 11:30:48 +0200

    adds getter for root but does nothing new

--- src/binary-trees.js
+++ src/binary-trees.js
@@ -8,39 +8,38 @@ const Tree = (array) => { //Module that holds all BST methods
   let root = null // Initialize root variable -- null if there's no array
   if (array) root = buildTree(array, 0, array.length - 1) // If an array is passed, create tree
   
-  const insert = (key) => { // Insert new node
-    let newNode = makeNode(key) // create new node
-    if (root === null) root = newNode // If root is null set root to new node
-    if (root !== null) insertNode(root, newNode) // if root isn't null make recursive call
-    return root
-  }
+  function insert(key){
+    let newNode = makeNode(key)
+    if (root === null) {
+      root = newNode
+    }else {
+      insertNode(root, newNode)
+    }
 
-  function insertNode(root, node) { // Recursive fn
-    if (node.key < root.key){ // If node value is less than root value
-      if (root.left === null){ // If left child is null
-        root.left = node // Set left node to new node
-      }else{
-        insertNode(root.left, node) // recurse the left branch
+  function insertNode(root, newNode){
+    if (newNode.key < root.key) {
+      if (root.left === null) {
+        root.left = newNode
+      }else {
+        insertNode(root.left, newNode)
       }
-    if (node.key > root.key){ // If node value is greater than root value
-      if (root.right === null) { // If right child is null
-        root.right = node // Set right node to new node
-      }else{
-        insertNode(root.right, node) // recurse the right branch
+    }
+    if (newNode.key > root.key) {
+      if (root.right === null) {
+        root.right = newNode
+      }else {
+        insertNode(root.right, newNode)
       }
     }
   }
-}
-
+  }
   return {
-    root,
+    get value(){return root},
     insert
   }
 }
 
 let a = Tree()
-a.insert(3)
-a.insert(4)
-a.insert(1)
-a.insert(13)
-console.log(a.root)
+console.log(a.insert(8))
+console.log(a.value())
+
