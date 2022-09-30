196c918af913c706c5014e619c81b6596bb0b983
doctype-Melvin <doctype.melvin@gmail.com>
2022-09-30 11:11:17 +0200

    adds linked list variables to app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log('Hello World!')
+const app = (() => {
+    // Linked List form controls
+        //Buttons
+    const headBtn = document.getElementById('setHead')
+    const appendBtn = document.getElementById('push')
+    const prependBtn = document.getElementById('unshift')
+    const indexBtn = document.getElementById('index')
+    const containsBtn = document.getElementById('hasNum')
+    const sizeBtn = document.getElementById('size')
+    const printHeadBtn = document.getElementById('printHead')
+    const tailBtn = document.getElementById('tail')
+    const popBtn = document.getElementById('pop')
+    const printBtn = document.getElementById('printList')
+
+        //Input fields
+    const head = document.getElementById('head')
+    const append = document.getElementById('append')
+    const prepend = document.getElementById('prepend')
+    const atIndex = document.getElementById('atIndex')
+    const contains = document.getElementById('contains')
+
+        //Output section
+    const listOutput = document.getElementById('listOutput')
+    
+})()
