b9866c5c06a541df45cea8e9fb29e00da5288e2b
doctype-Melvin <doctype.melvin@gmail.com>
2022-10-05 09:12:18 +0200

    <DIR>adds graph practice folder

---
+++ graph-practice/main.js
@@ -0,0 +1,9 @@
+/*
+A graph is a data structure that shares similar elements with a binary tree.
+The graph consists of vertices (also referred to as nodes; data) and edges, which are
+lines or arcs connecting any two vertices (nodes) in a graph.
+In conclusion a graph is composed of a set of vertices (V) and a set of edges (E) -
+G(E,V).
+Graphs can be directed or undirected. If edges have directions between vertices they are part of a directed graph - think
+Webpages. Whereas undirected graphs are like social networks.
+*/
